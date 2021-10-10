248db81537f39667525d58a1508be3e9b5abbc86
amadigot <90967992+amadigot@users.noreply.github.com>
2021-10-10 15:15:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "ODc4NTg3OTcwNTg2ODA4MzIx.YSDWzw.9XEgYhEK4dWRmS_I7m0Oz6Kf4no",
+    token: "ODc1NDA5OTQwNDA2MjE0NzQ3.YRVHCg.V4QGwFhhqT1uYKkTwC6aBmZWzto",
     prefix: "b!",
     owner: "622691164843409418",
     corona_api: "https://corona.lmao.ninja/v2"
